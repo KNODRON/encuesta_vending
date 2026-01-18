a71a8dc965fbcb5a40483b2ba7fc402f6af9c075
KNODRON <juan.jimenezv08@gmail.com>
2026-01-17 22:51:23 -0300

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "encuesta-vending-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
